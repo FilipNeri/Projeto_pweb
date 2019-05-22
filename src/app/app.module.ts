3987a755962c71d18f4c19bae18aea2eb738f7c5
FilipNeri <Filipe.neri50@gmail.com>
2019-05-22 19:32:56 -0300

    trabalho final

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { EstadosCadastroComponent } from './estados/estados-cadastro/estados-cadastro.component';
+import { EstadosPesquisaComponent } from './estados/estados-pesquisa/estados-pesquisa.component';
+import { EstadosModule } from './estados/estados.module';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ConfirmationService } from 'primeng/api';
+
+import { Routes, RouterModule } from '@angular/router';
+
+// const = cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura.
+const rotas: Routes = [
+  {path: 'estados', component: EstadosPesquisaComponent},
+  {path: 'estados/novo', component: EstadosCadastroComponent},
+  {path: 'estados/:id', component: EstadosCadastroComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    EstadosModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(rotas)
+  ],
+  providers: [
+    ConfirmationService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
